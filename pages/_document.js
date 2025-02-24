1f2beca4a989753422e27a0797dc61aeef219764
Frank Chen <fronk@Frank-the-Tank.local>
2025-02-24 00:13:10 -0800

    add document.js

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* External CSS, Fonts, Meta Tags, etc. */}
+          <link href="https://fonts.googleapis.com/css2?family=Canela+Text&family=Inconsolata:wght@200;300;400;500&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />  {/* This is where the page's content will be rendered */}
+          <NextScript />  {/* This renders the necessary scripts */}
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
